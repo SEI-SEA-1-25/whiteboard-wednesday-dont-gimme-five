ff7a58935ca7813e426c5bda3c074a0ef79a5795
mariiamel <mariiamelnikova96@gmail.com>
2021-03-31 10:21:06 -0700

    HW

--- solution.py
+++ solution.py
@@ -1,10 +1,12 @@
 
 def print_numbers():
-    numbers = int(input('Please give me two any numbers'))
-    num1, num2 = numbers.split()
+    num1, num2 = input('Please give me two any numbers').split()
+    num1 = int(num1)
+    num2 = int(num2)
 
-    for number in range(bum1, num2):
-        if number != '5'
-        print(number)
+    for num in range(num1, num2 + 1):
+        if not "5" in str(num):
+        print(num)
 
 print_numbers()
+
